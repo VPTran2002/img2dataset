f2dd2970fa475ade046944dab3bd9be245454256
Vinh Phuc Tran <vinhphuc.tran@tum.de>
2024-03-05 15:16:46 +0100

    in search for speed

--- img2dataset/DataManagement/Laion400mDataset.py
+++ img2dataset/DataManagement/Laion400mDataset.py
@@ -356,7 +356,7 @@ class Laion400mDataset(Dataset):
         self.__download_urls()
 
 def main():
-        l = Laion400mDataset(num_elements_per_caption=666667, batch_size_meta=4096, num_workers=6, shard_size=200000, thread_count=10)
+        l = Laion400mDataset(num_elements_per_caption=666667, batch_size_meta=4096, num_workers=12, shard_size=200000, thread_count=10)
 
 if __name__ == "__main__":
     main()    
